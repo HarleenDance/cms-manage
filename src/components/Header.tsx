27c4e537379afa2b97db684e887ae6a76f62d26d
Harleens <1040265283@qq.com>
2022-06-25 16:24:15 +0800

    添加头部

---
+++ src/components/Header.tsx
@@ -0,0 +1,19 @@
+/*
+ * @Descripttion: 
+ * @version: 18.1.2
+ * @Author: Harleens
+ * @Date: 2022-06-25 16:02:49
+ * @LastEditors: Harleens
+ * @LastEditTime: 2022-06-25 16:23:50
+ */
+import React, { useEffect, useState } from 'react'
+// import logoImg from '../assets/logo.png'
+import { Menu, Dropdown, message } from 'antd';
+import { CaretDownOutlined } from '@ant-design/icons';
+// import defaultAvatar from '../assets/defaultAvatar.jpg'
+
+export default function Header() {
+  return (
+    <div>Header</div>
+  )
+}
