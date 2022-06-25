13a40f618eb1b5a4a24248fa54590c306428075d
Harleens <1040265283@qq.com>
2022-06-25 17:12:23 +0800

    添加ts配置

---
+++ images.d.ts
@@ -0,0 +1,15 @@
+/*
+ * @Descripttion: 
+ * @version: 18.1.2
+ * @Author: Harleens
+ * @Date: 2022-06-25 17:08:06
+ * @LastEditors: Harleens
+ * @LastEditTime: 2022-06-25 17:08:09
+ */
+declare module '*.svg'
+declare module '*.png'
+declare module '*.jpg'
+declare module '*.jpeg'
+declare module '*.gif'
+declare module '*.bmp'
+declare module '*.tiff'
